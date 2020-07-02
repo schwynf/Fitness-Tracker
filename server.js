7c8baa9b2944d13396fa36b278ab65f629ce3738
Schwyn Francis <schwynf@gmail.com>
2020-07-02 16:53:32 -0700

    connection work

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds119618.mlab.com:19618/heroku_ghkz6hwr", { useNewUrlParser: true, useUnifiedTopology: true, useMongoClient:true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds119618.mlab.com:19618/heroku_ghkz6hwr", { useNewUrlParser: true, useUnifiedTopology: true, useMongoClient:true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(htmlController);
-app.use(workoutController)
+app.use(workoutController);
 app.listen(PORT, () => {
     console.log(`App running on port http://localhost:${PORT}`);
   });

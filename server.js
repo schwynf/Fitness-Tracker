4f4239ab7ff6adfd24906d135ef777f6048ffc9c
Schwyn Francis <schwynf@gmail.com>
2020-07-02 15:11:28 -0700

    fixing heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 const htmlController = require("./controllers/html-controller");
 const workoutController = require("./controllers/workout-controller");
 
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds119618.mlab.com:19618/heroku_ghkz6hwr", { useNewUrlParser: true, useUnifiedTopology: true, useMongoClient:true });
 
 app.use(htmlController);
 app.use(workoutController)
